78d42cdc56b87515b5c522199d2c799bc107e631
Emmanuel Onu <emmanuel.onu@robogarden.ca>
2023-08-11 16:07:18 -0600

    Created a tabs app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public alertButtons = ['OK', "Cancel", "Maybe"];
+
+  constructor(private alertController: AlertController, private toastController: ToastController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: 'This is an alert!',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('OK button clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Cancel button clicked');
+          }
+        }
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Hello World!',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
 
 }
